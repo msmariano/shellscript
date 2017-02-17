98b5e2c88858ada96ab98942a82dd8b001cee9e6
Marcelo dos Santos Mariano <marcelodossantosmariano@gmail.com>
2017-02-17 14:27:56 -0200

    Inserido validacao projeto

--- inserirgit.sh
+++ inserirgit.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-git init
-git add . 
-git commit -m 'initial commit'
-git remote add origin https://github.com/msmariano/$1.git
-git push -u origin master
+
+echo 'Informe o nome do projeto'
+read projeto
+if [ "$projeto" != "" ]  ;
+then
+	git init
+	git add . 
+	git commit -m 'initial commit'
+	git remote add origin https://github.com/msmariano/$projeto.git
+	git push -u origin master
+fi

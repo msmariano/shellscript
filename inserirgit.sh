50101aba1c14e7f43af986be025e53fcc065afda
Marcelo dos Santos Mariano <marcelodossantosmariano@gmail.com>
2017-02-17 14:46:02 -0200

    Colocado opcao para inserir ou fazer commit

--- inserirgit.sh
+++ inserirgit.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-echo 'Informe o nome do projeto'
+echo 'Informe o nome do projeto ou arquivo para fazer o commit'
 read projeto
+echo 'Informe a ação:  1 - criar 2 - commit'
+read acao
+
 if [ "$projeto" != "" ]  ;
 then
-	git init
-	git add . 
-	git commit -m 'initial commit'
-	git remote add origin https://github.com/msmariano/$projeto.git
-	git push -u origin master
-	echo 'Sucesso!'
+	if [ "$acao" = "1" ]
+	then
+		git init
+		git add . 
+		git commit -m 'initial commit'
+		git remote add origin https://github.com/msmariano/$projeto.git
+		git push -u origin master
+		echo 'Sucesso!'
+	else 
+		if [ "$acao" = "2" ]
+		then
+			git add $projeto
+			git commit
+			git push -u origin master
+		fi
+
+	fi
+	
 fi
+
+
